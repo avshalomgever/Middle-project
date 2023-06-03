047e45d922cdeb9d525014a7db3a9fc72ddcd968
avshalom evgi <avsha.em@gmail.com>
2023-06-03 11:11:41 +0300

    add changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,7 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-<<<<<<< HEAD
-import Event from "./components/event/Event";
-=======
+
 import EventPage from "./components/EventPage/EventPage";
-<<<<<<< HEAD
->>>>>>> c538ab0 (add event page)
-=======
->>>>>>> c538ab0e11e974bfac51ca232dfa6ef61c521dd7
->>>>>>> 12aeca2047dc294a19d187b9eef782c247ab5127
 import Layout from "./components/Layout/Layout";
 import HomePage from "./components/homePage/HomePage";
 import About from "./components/Aboutus/About";
